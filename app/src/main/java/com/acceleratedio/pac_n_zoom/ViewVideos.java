807df8046f4b9c461e078d633ffc599ab0485956
Joe <VectorVortec@gmail.com>
2015-11-18 00:48:44 -0700

    PnZ 1.1 - Plays Videos

--- app/src/main/java/com/acceleratedio/pac_n_zoom/ViewVideos.java
+++ app/src/main/java/com/acceleratedio/pac_n_zoom/ViewVideos.java
@@ -1,5 +1,7 @@
 package com.acceleratedio.pac_n_zoom;
 
+import android.content.Context;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.PixelFormat;
@@ -14,9 +16,11 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 import android.content.Intent;
-
+import android.media.MediaPlayer.OnPreparedListener;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -27,7 +31,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -37,124 +40,121 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-
 import de.greenrobot.event.EventBus;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
 
+public class ViewVideos extends Activity implements SurfaceHolder.Callback {
 
-//Implement SurfaceHolder interface to Play video
-//Implement this interface to receive information about changes to the surface
-public class ViewVideos extends Activity implements SurfaceHolder.Callback{
+    MediaPlayer mediaPlayer;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    boolean pausing = false;
+    ProgressDialog progress;
+    VideoView mVideoView;
+    Button bback;
+    private int position = 0;
+    Button mPlay;
 
-	MediaPlayer mediaPlayer;
-	SurfaceView surfaceView;
-	SurfaceHolder surfaceHolder;
-	boolean pausing = false;;
-	public static ProgressDialog progress;
-
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.viewvideo);
-
-		getWindow().setFormat(PixelFormat.UNKNOWN);
-
-		progress = ProgressDialog.show(this, "Loading the animation", "dialog message", true);
-		MakePostRequest get_video = new MakePostRequest();
-    get_video.execute();
-  }
-
-  public class MakePostRequest extends AsyncTask<String, Void, String> {
     @Override
-    protected String doInBackground(String... urls) {
-			String vidFileName;
-			InputStream in = null;
-			String result = "fail";
-			int position = getIntent().getIntExtra("position", -1);
-			vidFileName = PickAnmActivity.fil_nams[position].replace('/', '?') + ".mp4";
-			String httpAddrs = "https://meme.svgvortec.com/Droid/db_rd.php?";
-			httpAddrs += vidFileName; 
-			File sdCard = Environment.getExternalStorageDirectory(); // Find sdcard
-			File dir = new File (sdCard.getAbsolutePath() + "/acceleratedio/pacnzoom");
-			dir.mkdirs();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewvideo);
+        int position = getIntent().getIntExtra("position", -1);
+        String vidFileName = PickAnmActivity.fil_nams[position].replace('/', '?') + ".mp4";
+        String httpAddrs = "https://meme.svgvortec.com/Droid/db_rd.php?";
+        httpAddrs += vidFileName;
+        mVideoView = (VideoView) findViewById(R.id.vviidd);
+				//mVideoView.setZOrderMediaOverlay(true);
+				//mVideoView.setZOrderOnTop(true);
+				mVideoView.setVisibility(View.VISIBLE);
+				progress = new ProgressDialog(ViewVideos.this);
+				progress = ProgressDialog.show(this, "Loading the video", "dialog message", true);
+				progress.show();
+				progress.setCancelable(true);
 
-			try {
-				HttpClient httpClient = new DefaultHttpClient();
-				HttpGet httpRequest = new HttpGet(httpAddrs);
-				HttpResponse response = httpClient.execute(httpRequest);
-				in = response.getEntity().getContent();
+        try {
+					// Start the MediaController
+					MediaController mediacontroller = new MediaController(ViewVideos.this);
+					mediacontroller.setMediaPlayer(mVideoView);
+					mediacontroller.setAnchorView(mVideoView);
+					mVideoView.setMediaController(mediacontroller);
+					surfaceHolder = mVideoView.getHolder();
+					Uri video = Uri.parse(httpAddrs);
+					mVideoView.setVideoURI(video);
+					getWindow().setFormat(PixelFormat.TRANSLUCENT);
 
-				File file = new File(dir, "/file.mp4"); // Instantiate the file class
-				FileOutputStream f = new FileOutputStream(file); // Open the file
+					mVideoView.setOnPreparedListener(new OnPreparedListener() {
 
-				byte[] buffer = new byte[1024];
-				int len1 = 0;
+						public void onPrepared(MediaPlayer mp) {
 
-				while ((len1 = in.read(buffer)) > 0) {                          
-						f.write(buffer, 0, len1);               
-				}
+							if (mp.getVideoHeight() == 0) {
 
-				f.close();
-				progress.dismiss();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				progress.dismiss();
-			} finally {
-				progress.dismiss();
-				if (in != null) {
-					try {
-						in.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
+								mp.setOnVideoSizeChangedListener(new OnVideoSizeChangedListener() {
 
-			return result;
-		}
+                  @Override
+                  public void onVideoSizeChanged(MediaPlayer mp, int arg1, int arg2) {
+                    
+										// TODO Auto-generated method stub
+										progress.dismiss();
+										mVideoView.requestFocus();
+										mVideoView.start();
+                  }
+                });
+              } else { 
+								progress.dismiss();
+								mVideoView.requestFocus();
+								mVideoView.start();
+							}
+						}
+					});           
 
-		protected void onPostExecute(String vidFileName) {
-			super.onPostExecute(vidFileName);
-     	dsply_video(vidFileName);
-		}
-	}
-	
-	private void dsply_video(String video) {
-		Log.d("dsply_video", "The video is loaded.");
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
 
-		//Displays a video file.   
-		VideoView mVideoView = (VideoView)findViewById(R.id.vviidd);
-		File sdCard = Environment.getExternalStorageDirectory(); // Find sdcard
-		File dir = new File (sdCard.getAbsolutePath() + "/acceleratedio/pacnzoom");
-		File file = new File(dir, "/file.mp4"); // Instantiate the file class
-		Uri uri = Uri.fromFile(file);
-		mVideoView.setVideoURI(uri);
-		mVideoView.requestFocus();
-		mVideoView.start();
-	}
+        bback = (Button) this.findViewById(R.id.bback);
+        bback.setOnClickListener(new OnClickListener() {
 
-	public void bbaacckk(View view){
-		Intent intent = new Intent(this, PickAnmActivity.class);
-		startActivity(intent);
-	}
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 
-	public class AlphaListUpdateEvent{ 
-	}
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("Position", mVideoView.getCurrentPosition());
+        mVideoView.pause();
+    }
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-		int height) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        position = savedInstanceState.getInt("Position");
+        mVideoView.seekTo(position);
+    }
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-	}
+    public class AlphaListUpdateEvent{
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+ 
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+         
+    }		
 }
